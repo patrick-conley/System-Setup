f4756c8594219ca14221dc5f166e4cf1c3a412a0
Patrick Conley <patrick.bj.conley@gmail.com>
2012-07-05 13:44:15 -0700

    crontab completion output; README spelling fixes

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Patrick Conley <pconley@uvic.ca>
-# Last modified: 2012 Jan 27
+# Last modified: 2012 Jul 05
 #
 # Summary: Set up a default crontab, calling batch_unison.sh (hourly),
 # podcaster.pl (weekly)
@@ -19,3 +19,4 @@ $(( $RANDOM % 60 )) * * * * /home/pconley/bin/sync/batch_unison.sh -t 1
 $(( $RANDOM % 60 )) 15 * * 0 podcaster.pl
 EOT
 
+echo "Successfully added crontab items"
