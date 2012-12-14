e0bd9050aea8ad19a0a2aab96e4a1b6216b47329
Patrick Conley <patrick.bj.conley@gmail.com>
2012-12-13 16:55:11 -0800

    Translated tmux install script from zsh to sh

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -1,19 +1,21 @@
-#!/usr/bin/zsh
+#!/bin/bash
 
 # Patrick Conley <pconley@uvic.ca>
-# Last modified: 2012 Oct 31
+# Last modified: 2012 Dec 13
 #
 # Summary: Install tmux v. 1.7
 
 # Identify the version of tmux installed by the package manager
 VERSION=$( apt-cache showpkg tmux | grep -A1 Versions: | grep -o "^[0-9\.]*" )
 
-if (( $VERSION >= 1.7 ))
+if [[ ! $( echo "$VERSION >= 1.7" | bc -q ) ]]
 then
    apt-get install tmux 
    exit 0
 else
+   echo -en "\e[0;31m"
    echo "tmux via apt-get ($VERSION) is too old. Installing manually"
+   echo -en "\e[0m"
 fi
 
 # install dependencies
